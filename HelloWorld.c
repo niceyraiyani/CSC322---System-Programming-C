fd440ebb1992e50448baf3deda5708e14042cf1e
niceyraiyani <npr39@miami.edu>
2022-11-06 19:27:01 -0500

    added very crool

--- HelloWorld.c
+++ HelloWorld.c
@@ -3,7 +3,7 @@
 //-----------------------------------------------------------------------------
 int main(void) {
 
-    printf("Hello crool World!\n");
+    printf("Hello Very Crool World!\n");
 
     return(EXIT_SUCCESS);
 }
